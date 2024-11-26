0129c76fc3262dd0bf377e40eae115fffe2459fc
unknown <67873431+Ackermiam@users.noreply.github.com>
2024-11-26 17:03:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="Home">
+
+    <div ref="scene" class="Scene"></div>
+
+    <div class="Title">
+      <h1
+        v-for="(_, index) in 4"
+        :key="index"
+        :style="{ opacity: `${1 - index / 3.5}` }"
+      >
+        Acker'Prod
+      </h1>
+    </div>
+  </section>
+  <section class="HomeTwo"></section>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { Logic } from "./three/scene";
+
+const scene = ref();
+let logic;
+
+onMounted(() => {
+  logic = new Logic(scene.value);
+});
+</script>
+
+<style scoped>
+.Home {
+  width: 100vw;
+  height: 300vh;
+  position: relative;
+}
+.HomeTwo {
+  width: 100vw;
+  height: 300vh;
+  background: pink;
+  z-index: 3;
+}
+
+.Scene {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  z-index: 2;
+}
+
+.Title {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 50px;
+  z-index: -1;
+}
+
+h1 {
+  font-size: 13vw;
+  font-family: "Mewatonia";
+  line-height: 0.8em;
+  margin: 0;
+  color: rgb(255, 255, 255);
+}
+
+@media (max-width: 900px) {
+  h1 {
+    font-size: 18vw;
+  }
+
+  .Title {
+    top: 200px;
+  }
+}
+</style>
---
+++ src/three/hole.ts
@@ -0,0 +1,51 @@
+import { Object3D, AnimationMixer, Clock } from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+export class BlackHole {
+  mesh: Object3D;
+  loader: GLTFLoader;
+  clock = new Clock();
+  animation: any;
+  mixer: AnimationMixer | null = null;
+  width: number;
+
+  constructor(width: number) {
+    this.loader = new GLTFLoader();
+    this.mesh = new Object3D();
+    this.width = width;
+  }
+
+  tick(xPos: number) {
+    if (this.mixer) {
+      const delta = this.clock.getDelta();
+      this.mixer.update(delta);
+    }
+    this.moveHole(xPos);
+  }
+
+  async loadMesh() {
+    const gltf = await this.loader.loadAsync(
+      "/portfolio/models/dark/scene.gltf"
+    );
+    this.mesh = gltf.scene;
+    this.animation = gltf.animations;
+    this.mesh.scale.set(3, 3, 3);
+    this.mesh.position.y = 1;
+
+    if(this.width < 900) {
+      this.mesh.scale.set(2, 2, 2);
+      this.mesh.position.y = 1.5;
+    }
+
+    if (gltf.animations.length > 0) {
+      this.mixer = new AnimationMixer(this.mesh);
+      const action = this.mixer.clipAction(gltf.animations[0]);
+      action.play();
+    }
+  }
+
+  moveHole(xPos: number) {
+    const normalize = (xPos / this.width) * 2 - 1;
+    this.mesh.rotation.z = normalize / 20;
+  }
+}
